c29f63802b01492ad3b8648f98679461b16bea7c
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-10-12 22:31:16 -0300

    refactor robot in a grid code
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- src/main/java/recursion_and_dp/RobotInAGrid.java
+++ src/main/java/recursion_and_dp/RobotInAGrid.java
@@ -8,23 +8,23 @@ public class RobotInAGrid {
   private static List<Point> path;
 
   private static boolean dfs(int[][] grid, int r, int c, int i, int j) {
-    if (i == (r - 1) && j == (c - 1)) return true;
+    if (i == 0 && j == 0) return true;
 
     marked[i][j] = true;
 
     boolean pathA = false, pathB = false;
 
-    if (i + 1 < r && !marked[i + 1][j] && grid[i + 1][j] == 0)
-      pathA = dfs(grid, r, c, i + 1, j);
+    if (i - 1 >= 0 && !marked[i - 1][j] && grid[i - 1][j] == 0)
+      pathA = dfs(grid, r, c, i - 1, j);
 
-    if (j + 1 < c && !marked[i][j + 1] && grid[i][j + 1] == 0)
-      pathB = dfs(grid, r, c, i, j + 1);
+    if (j - 1 >= 0 && !marked[i][j - 1] && grid[i][j - 1] == 0)
+      pathB = dfs(grid, r, c, i, j - 1);
 
     if (pathA || pathB) {
       if (pathA)
-        path.add(new Point(i + 1, j));
+        path.add(new Point(i - 1, j));
       else
-        path.add(new Point(i, j + 1));
+        path.add(new Point(i, j - 1));
 
       return true;
     }
@@ -40,9 +40,9 @@ public class RobotInAGrid {
       for (int j = 0; j < c; j++)
         marked[i][j] = false;
     
-    boolean result = dfs(grid, r, c, 0, 0);
+    boolean result = dfs(grid, r, c, r - 1, c - 1);
 
-    path.add(new Point(0, 0));
+    path.add(new Point(r - 1, c - 1));
 
     return result ? path : null; 
   }
--- src/test/java/recursion_and_dp/RobotInAGridTest.java
+++ src/test/java/recursion_and_dp/RobotInAGridTest.java
@@ -18,7 +18,7 @@ public class RobotInAGridTest {
 
     assertEquals(
       RobotInAGrid.run(grid, 5, 5).toString(),
-      "[(4, 4), (4, 3), (3, 3), (2, 3), (1, 3), (1, 2), (0, 2), (0, 1), (0, 0)]"
+      "[(0, 0), (0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (3, 3), (4, 3), (4, 4)]"
     );
   }
 
